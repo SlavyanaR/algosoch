a86f2f8223d57aa7551a3d9c2eef01dddd677b33
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2023-03-23 20:38:53 +0300

    23/3/23

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "a859in",
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'http://localhost:3000' 
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
     },
   },
 });
