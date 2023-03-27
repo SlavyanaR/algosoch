f841dab2eb6b60b76af4387ff0bd485dc8ea3807
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2023-03-27 21:44:45 +0300

    27/3/23_1

--- src/components/ui/circle/circle.tsx
+++ src/components/ui/circle/circle.tsx
@@ -13,6 +13,17 @@ interface CircleProps {
   isSmall?: boolean;
 }
 
+interface CircleProps {
+  state?: ElementStates;
+  letter?: string;
+  head?: string | React.ReactElement | null;
+  index?: number;
+  tail?: string | React.ReactElement | null;
+  tailType?: "string" | "element";
+  extraClass?: string;
+  isSmall?: boolean;
+}
+
 export const Circle: React.FC<CircleProps> = ({
   state = ElementStates.Default,
   letter,
@@ -25,21 +36,20 @@ export const Circle: React.FC<CircleProps> = ({
   return (
     <div className={`${styles.content} ${extraClass}`}>
       <div
-        className={`text text_type_input text_color_input mb-4 ${
-          styles.absolute
-        } ${styles.head} ${
-          styles[typeof head === "string" ? "string" : "element"]
-        }`}
+        className={`text text_type_input text_color_input mb-4 ${styles.absolute
+          } ${styles.head} ${styles[typeof head === "string" ? "string" : "element"]
+          }`}
       >
         {head}
       </div>
       <div
-        className={`${styles.circle}  ${isSmall ? styles.small : ""} ${
-          styles[state]
-        }`}
+        data-testid="circle_border"
+        className={`${styles.circle}  ${isSmall ? styles.small : ""} ${styles[state]
+          }`}
       >
         <p
           className={`text text_type_circle text_color_input ${styles.letter}`}
+          data-testid="circle_text"
         >
           {letter}
         </p>
@@ -50,11 +60,9 @@ export const Circle: React.FC<CircleProps> = ({
         {index?.toString()}
       </p>
       <div
-        className={`text text_type_input text_color_input mt-4 ${
-          styles.absolute
-        } ${index?.toString() ? styles.tail60 : styles.tail30} ${
-          styles[typeof tail === "string" ? "string" : "element"]
-        }`}
+        className={`text text_type_input text_color_input mt-4 ${styles.absolute
+          } ${index?.toString() ? styles.tail60 : styles.tail30} ${styles[typeof tail === "string" ? "string" : "element"]
+          }`}
       >
         {tail}
       </div>
